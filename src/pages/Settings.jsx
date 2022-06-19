2bd78d7412add5f8c882f398ebee029016a748aa
Rosamaria Rodriguez <rosamaria.developer@gmail.com>
2022-06-19 15:35:12 -0500

    Create Setting page

---
+++ src/pages/Settings.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ThemeMenu from '../components/molecules/thememenu/ThemeMenu'
+
+const Settings = () => {
+    
+    return (
+        <div>
+            <h2 className="page-header">
+                Configuración de perfil
+            </h2>
+        </div>
+    )
+}
+
+export default Settings
